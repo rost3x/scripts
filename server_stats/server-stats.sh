590703c7b07654d58925f97c87d90c492f1fbe6f
rost3x <ajrostek@gmail.com>
2025-01-04 21:49:48 +0000

    Added total GPU usage

--- server_stats/server-stats.sh
+++ server_stats/server-stats.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Display total CPU usage
+echo "+-------------------------------+"
+echo "|Total CPU usage		|"
+echo "+-------------------------------+"
+
+top -bn1 | grep '%Cpu(s)'| cut -d ',' -f 4 | awk '{print 100-$1 "%"}'
+
 # Display top 5 processes used by the cpu
 echo "+-------------------------------+"
 echo "|Top 5 CPU processes		|"
