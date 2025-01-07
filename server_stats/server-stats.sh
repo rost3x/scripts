7a39000485f2b3bf08f3edf6ca962569863a7907
rost3x <ajrostek@gmail.com>
2025-01-07 21:41:34 +0000

    Added total memory usage stats

--- server_stats/server-stats.sh
+++ server_stats/server-stats.sh
@@ -7,6 +7,13 @@ echo "+-------------------------------+"
 
 top -bn1 | grep '%Cpu(s)'| cut -d ',' -f 4 | awk '{print 100-$1 "%"}'
 
+#Display total memory usage including percentage
+echo "+-------------------------------+"
+echo "|Total memory usage		|"
+echo "+-------------------------------+"
+
+df -h --total | tail -1 | awk '{ print "Free: " $4 "\n" "Used: " $3 " / " $5 }'
+
 # Display top 5 processes used by the cpu
 echo "+-------------------------------+"
 echo "|Top 5 CPU processes		|"
